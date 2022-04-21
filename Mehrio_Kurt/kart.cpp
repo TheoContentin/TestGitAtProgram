11775f11bdeb2ccf41d74fa0e949dbc045d677bd
Théo Contentin <contentin.theo@gmail.com>
2022-04-21 11:20:59 +0200

    Kart

--- Mehrio_Kurt/kart.cpp
+++ Mehrio_Kurt/kart.cpp
@@ -3,4 +3,35 @@
 kart::kart(map map){
     vit = {map.start_direction[0],map.start_direction[1]};
     pos = map.start_position;
+    moteur = 0;
+    dir = 0;
+    FloatPoint3 fB[8]={FloatPoint3(3,-1,-1),FloatPoint3(5,-1,-1),FloatPoint3(5,1,-1),FloatPoint3(3,1,-1),   // constructor (float)
+                       FloatPoint3(3,-1,1),FloatPoint3(5,-1,1),FloatPoint3(5,1,1),FloatPoint3(3,1,1)
+                      };
+    fP = fB;
 }
+
+void kart::showCube(){
+    Triangle fT[12]={Triangle(0,2,1),Triangle(2,0,3),Triangle(4,5,6),Triangle(6,7,4),   // A cube
+                     Triangle(0,1,5),Triangle(5,4,0),Triangle(1,2,6),Triangle(6,5,1),
+                     Triangle(2,3,7),Triangle(7,6,2),Triangle(3,0,4),Triangle(4,7,3)
+                    };
+    Mesh fM(fP,8,fT,12);
+    showMesh(fM);
+}
+
+void kart::depl(){
+    double angle = dir*0.5;
+    vit.x() = vit.x()*cos(angle)-1*sin(angle)*vit.y();
+    vit.y() = vit.x()*sin(angle) + vit.y()*cos(angle);
+
+    for (int i=0; i<8;i++){
+        fP[i].x() += vit.x()*moteur;
+        fP[i].y() += vit.y()*moteur;
+    }
+}
+
+void kart::updateKeys(){
+
+}
+
--- Mehrio_Kurt/kart.h
+++ Mehrio_Kurt/kart.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Imagine/Images.h>
 #include "map.h"
+#include <cmath>
 
 using namespace Imagine;
 
@@ -8,5 +9,10 @@ class kart{
      FVector<float, 3> pos;
      FVector<float, 2> vit;
      int dir; //-1 si il tourne à gauche, 1 à droite, 0 tout droit
+     int moteur; //1 si en marche, 0 sinon, -1 marche arriere
      kart(map map);
+     FloatPoint3* fP;
+     void showCube();
+     void depl();
+     void updateKeys();
 };
